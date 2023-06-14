c84cbca51364efd3dd6492a44a540e24f386cdea
Bruno Benavent <brunobenavent@gmail.com>
2023-06-14 16:43:25 +0200

    commit

--- src/context/PacientesProvider.jsx
+++ src/context/PacientesProvider.jsx
@@ -3,14 +3,20 @@ import clienteAxios from "../config/axios";
 
 
 const PacientesContext = createContext()
-
+    
 
 export const PacientesProvider = ({children}) => {
+    const [pacientes, setPacientes] = useState([])
+
+
+    const guardarPaciente = async paciente => {
+        console.log(paciente)
+    }
     return(
         <PacientesContext.Provider
             value={{
-                
-
+                pacientes,
+                guardarPaciente
             }}
         >
             {children}
